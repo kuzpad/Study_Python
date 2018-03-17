fbbefcbc0f403118661b577f5affe421ca9b8b84
Kazuya Hirata <kazuya.hirata.sd@gmail.com>
2018-03-17 00:39:34 -0400

    to add repeating function

--- open_close_application.py
+++ open_close_application.py
@@ -1,6 +1,10 @@
 import subprocess
 import time
 #fresult = subprocess.run('touch test.txt', shell=True)
-result = subprocess.run('open /Applications/LINE.app', shell=True)
-time.sleep(5) #5秒まつ
-result = subprocess.run('killall LINE', shell=True)
+cnt = 0
+while cnt < 5:
+	result = subprocess.run('open /Applications/LINE.app', shell=True)
+	time.sleep(5) #5秒まつ
+	result = subprocess.run('killall LINE', shell=True)
+	cnt += 1
+	print(cnt)
